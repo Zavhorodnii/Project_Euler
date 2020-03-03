61446d93241df0e574fe7254335b9a5b99f20f7e
Виталий Завгородний <devoir98@gmail.com>
2020-03-03 22:08:44 +0200

    Fix Exe_3

--- Exe_3.py
+++ Exe_3.py
@@ -1,15 +1,16 @@
 import math
+from subprocess import Popen, PIPE
 from multiprocessing import Process, Queue
 
 allProcesses = list()
 
 
 def run(long, max, min, q, bool):
-    write = False
     while max >= min:
         try:
             if long % max == 0:
                 if bool and max != 1:
+                    print(max)
                     write = run(max, max-1, min, q, False)
                 if not bool:
                     if max == 1:
@@ -22,16 +23,26 @@ def run(long, max, min, q, bool):
         except Exception:
             pass
         max -= 1
+    if not bool:
+        print("long = ", long)
+        return True
 
 
 def start():
     q = Queue()
     result = list()
-
-    long_var = int(input("Enter value: "))
-    thread_var = int(input("Enter thread count: "))
+    while True:
+        try:
+            long_var = int(input("Enter value: "))
+            thread_var = int(input("Enter thread count: "))
+            if thread_var > long_var or long_var < 2 or thread_var < 2:
+                print("Error")
+            else:
+                break
+        except Exception:
+            pass
     var_1 = math.ceil(long_var / thread_var)
-    var_2 = 0
+    var_2 = 1
     while True:
         if var_2 + var_1 >= long_var:
             create(long_var, long_var-1, var_2, q)
@@ -45,7 +56,7 @@ def start():
     for p in allProcesses:
         p.join()
 
-
+    result.append(1)
     for i in range(q.qsize()):
         result.append(q.get())
 
