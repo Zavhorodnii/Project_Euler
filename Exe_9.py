b559c39ddbd0b489cd6074a7ef48e9013b2245ac
Виталий Завгородний <devoir98@gmail.com>
2020-03-05 21:28:44 +0200

    Exe_9 ready

--- Exe_9.py
+++ Exe_9.py
@@ -1,23 +1,22 @@
+from datetime import datetime
+
 
 def start():
     var = int(input("Enter value: "))
-    point_1 = var
+    time = datetime.now()
+    point_1 = int(var/2)
     while True:
         point_2 = point_1 - 1
         while True:
-            point_3 = point_2 - 1
-            while True:
-                if point_3 + point_2 + point_1 == var:
-                    if point_3**2 + point_2**2 == point_1**2:
-                        print(point_3 * point_2 * point_1)
-                        return
-                if point_3 == 0:
-                    break
-                point_3 -= 1
+            point_3 = var - (point_2 + point_1)
+            if point_3**2 + point_2**2 == point_1**2:
+                print("{} + {} = {}".format(point_3, point_2, point_1))
+                print(point_3 * point_2 * point_1)
+                print("time: ", datetime.now()-time)
+                return
             if point_2 == 1:
                 break
             point_2 -= 1
-
         if point_1 == 2:
             break
         point_1 -= 1
